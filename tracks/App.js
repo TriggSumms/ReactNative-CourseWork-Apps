b38d75543acccc69ebb1cb1433441558c7c0317e
Trigg Summs <stsumms@gmail.com>
2021-02-01 17:01:14 -0600

    long intro to react native customizable elements and my applications Navigator has been built

--- tracks/App.js
+++ tracks/App.js
@@ -1,22 +1,71 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-// import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-// import { createStackNavigator } from 'react-navigation-stack';
-// import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+// import {
+//   createAppContainer,
+//   createStackNavigator,
+//   createBottomTabNavigator,
+//   createSwitchNavigator
+// } from 'react-navigation';
+import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
+// import { Provider as AuthProvider } from './src/context/AuthContext';
+// import { setNavigator } from './src/navigationRef';
+// import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
+// import { Provider as LocationProvider } from './src/context/LocationContext';
+// import { Provider as TrackProvider } from './src/context/TrackContext';
+// import { FontAwesome } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+//First step is to make a user sign in/up ....creating a navigation or logical user "flow"
+const switchNavigator = createSwitchNavigator({
+  ResolveAuth: ResolveAuthScreen,
+  loginFlow: createStackNavigator({
+    //toggling between "views"
+    Signup: SignupScreen,
+    Signin: SigninScreen
+  }),
+  //Create the nav's routes... (*assinging bottom tabs)
+  mainFlow: createBottomTabNavigator({
+    trackListFlow,
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen
+  })
 });
+
+const App = createAppContainer(switchNavigator);
+
+
+const trackListFlow = createStackNavigator({
+  TrackList: TrackListScreen,
+  TrackDetail: TrackDetailScreen
+});
+
+trackListFlow.navigationOptions = {
+  title: 'Tracks',
+  tabBarIcon: <FontAwesome name="th-list" size={20} />
+};
+
+
+
+export default () => {
+  return (
+    <TrackProvider>
+      <LocationProvider>
+        <AuthProvider>
+          <App
+            ref={navigator => {
+              setNavigator(navigator);
+            }}
+          />
+        </AuthProvider>
+      </LocationProvider>
+    </TrackProvider>
+  );
+};
--- tracks/src/screens/AccountScreen.js
+++ tracks/src/screens/AccountScreen.js
@@ -7,15 +7,15 @@ import { Context as AuthContext } from '../context/AuthContext';
 import { FontAwesome } from '@expo/vector-icons';
 
 const AccountScreen = () => {
-  const { signout } = useContext(AuthContext);
+ // const { signout } = useContext(AuthContext);
 
   return (
-    <SafeAreaView forceInset={{ top: 'always' }}>
+    // <SafeAreaView forceInset={{ top: 'always' }}>
       <Text style={{ fontSize: 48 }}>AccountScreen</Text>
-      <Spacer>
-        <Button title="Sign Out" onPress={signout} />
-      </Spacer>
-    </SafeAreaView>
+    //   <Spacer>
+    //     <Button title="Sign Out" onPress={signout} />
+    //   </Spacer>
+    // </SafeAreaView>
   );
 };
 
